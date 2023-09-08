77a37bae3f63d44355d1c2c2ab73a3620c791b02
Crowdbotics <team@crowdbotics.com>
2023-09-08 19:16:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "treeai-43288",
+  url: "https://treeai-43288.botics.co",
+  api: "https://treeai-43288.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
